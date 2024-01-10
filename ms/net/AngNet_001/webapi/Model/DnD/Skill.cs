ae7adf191e1ba9665b53e6883e374f0f992b3e5a
gusucrerey <113461634+gusucrerey@users.noreply.github.com>
2024-01-09 19:39:19 -0700

    just some more D&D stuff because Im bored

---
+++ ms/net/AngNet_001/webapi/Model/DnD/Skill.cs
@@ -0,0 +1,36 @@
+﻿namespace webapi.Model.DnD {
+   public class Skill {
+      private string _name = string.Empty;
+      private string _description = string.Empty;
+      private AttributeType _rollAttribute = AttributeType.STR;
+
+      public Skill(string Name, AttributeType RollAttribute, string Description = "") {
+         _name = Name;
+         _description = Description;
+         _rollAttribute = RollAttribute;
+      }
+
+      public static List<Skill> GetBaseSkillList() {
+         var list = new List<Skill>();
+         list.Add(new Skill("Athletics", AttributeType.STR));
+         list.Add(new Skill("Acrobatics", AttributeType.DEX));
+         list.Add(new Skill("Acrobatics", AttributeType.DEX));
+         list.Add(new Skill("Acrobatics", AttributeType.DEX));
+         list.Add(new Skill("Arcana", AttributeType.INT));
+         list.Add(new Skill("Arcana", AttributeType.INT));
+         list.Add(new Skill("Arcana", AttributeType.INT));
+         list.Add(new Skill("Arcana", AttributeType.INT));
+         list.Add(new Skill("Arcana", AttributeType.INT));
+         list.Add(new Skill("Animal Handling", AttributeType.WIS));
+         list.Add(new Skill("Animal Handling", AttributeType.WIS));
+         list.Add(new Skill("Animal Handling", AttributeType.WIS));
+         list.Add(new Skill("Animal Handling", AttributeType.WIS));
+         list.Add(new Skill("Animal Handling", AttributeType.WIS));
+         list.Add(new Skill("Deception", AttributeType.CHA));
+         list.Add(new Skill("Intimidation", AttributeType.CHA));
+         list.Add(new Skill("Performance", AttributeType.CHA));
+         list.Add(new Skill("Persuasion", AttributeType.CHA));
+         return list;
+      }
+   }
+}
