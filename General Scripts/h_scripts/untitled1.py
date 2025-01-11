124b130548947c51c07836e7726e940b1722cd5e
gusucrerey <113461634+gusucrerey@users.noreply.github.com>
2025-01-11 11:21:00 -0700

    updates

---
+++ General Scripts/h_scripts/untitled1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 08:49:51 2025
+
+@author: sucre
+"""
+'''
+I am more than my physical body
+Because I am more than physical matter, 
+I can perceive that which is greater than the physical world.
+
+Therefore, I deeply desire:
+to expand, to experience, to know, to understand, to control, 
+and to use such greater energies and energy systems 
+as may be beneficial and constructive to me 
+and to those who follow me
+
+Also, I deeply desire the help and cooperation, 
+the assistance, and the understanding 
+of those individuals 
+whose wisdom, development, and experience 
+are equal to or greater than my own
+
+I ask for their guidance and protection 
+from any influence or any source 
+that might provide me with less than my stated desires
+'''
