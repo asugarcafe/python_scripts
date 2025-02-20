ee14e492bf25577e16a80ef1ad81402c64c7ced3
gusucrerey <113461634+gusucrerey@users.noreply.github.com>
2025-02-20 15:36:04 -0700

    aertwert

--- General Scripts/h_scripts/hscript_reader.py
+++ General Scripts/h_scripts/hscript_reader.py
@@ -92,15 +92,15 @@ https://freehypnosisscripts.com/subject-scripts/creative-abilities/
 loop = 100
 file_path = "r_single.line.repeater.txt"
 #file_path = 'r_priming.ap.txt'
-#file_path = "r_visualization.improvement.txt"
+file_path = "r_visualization.improvement.txt"
 #file_path = 'r_mnemonics.electrical.txt'
-file_path = 'r_mentalacuity.txt'
-# # file_path = 'r_manifesting.feedback.txt'
+#file_path = 'r_mentalacuity.txt'
+#file_path = 'r_manifesting.feedback.txt'
 #file_path = 'r_reading.comprehension.pegs.txt'
 #file_path = 'r_time.distortion.txt'
 #file_path = 'r_memory.txt'
 #file_path = 'r_memory.card.pegs.txt'
-file_path = 'r_manifestation.healing.txt'
+#file_path = 'r_manifestation.healing.txt'
 
 
 desired_outcome = "to " + "slow down time at will"
